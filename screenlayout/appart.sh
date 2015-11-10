de581b809357a970e7d587f0600ce05864799313
thehunt33r <matthieuinspain@gmail.com>
2015-11-10 11:32:49 +0100

    Added tmux. Added tmux powerline

--- screenlayout/appart.sh
+++ screenlayout/appart.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 1432x1080 --rotate normal --output DP1 --off --output DP2-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP2-2 --off --output HDMI1 --off --output HDMI3 --off --output HDMI2 --off --output DP1-1 --off --output VGA1 --off --output DP1-8 --mode 2560x1080 --pos 1080x0 --rotate normal --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 2752x1080 --rotate normal --output DP1 --off --output DP1-1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output DP1-8 --mode 2560x1080 --pos 2408x0 --rotate normal --output DP2 --mode 1920x1080 --pos 1328x0 --rotate left
