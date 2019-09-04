d4cf2c0fd8e0bc2e2e0d67cc9614885946f7caa0
Matthieu Antoine <matthieu.antoine@n26.com>
2019-09-04 13:56:25 +0200

    Cleanup

--- py3lock/install_py3lock.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo ln -sn `pwd`/py3lock /usr/bin/py3lock
--- screenlayout/appart.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 2752x1080 --rotate normal --output DP1 --off --output DP1-1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output DP1-8 --mode 2560x1080 --pos 2408x0 --rotate normal --output DP2 --mode 1920x1080 --pos 1328x0 --rotate left
